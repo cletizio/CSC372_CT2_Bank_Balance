2aa20061b45c61e6b39475813279505f9efc3663
Chris Letizio <cletizio@gmail.com>
2023-10-22 11:11:55 -0400

    Added Deposit button

--- src/BankingFrame.java
+++ src/BankingFrame.java
@@ -13,6 +13,8 @@ public class BankingFrame extends JFrame implements ActionListener {
     private JTextField finalBalanceField;
     private JLabel finalBalanceLabel;
     private JButton processButton;
+    private JButton depositButton;
+    private JButton withdrawButton;
     private JPanel panel;
 
     BankingFrame() {
@@ -41,19 +43,34 @@ public class BankingFrame extends JFrame implements ActionListener {
         finalBalanceField.setEditable(false);
         finalBalanceField.setText("0");
 
-        processButton = new JButton("Process transactions");
-        processButton.addActionListener(this);
+        depositButton = new JButton("Deposit");
+
+
+        withdrawButton = new JButton("Withdraw");
+
+
+        depositButton.addActionListener(new ActionListener() {
+            public void actionPerformed (ActionEvent e){
+                double balance = Double.parseDouble(balanceField.getText());
+                double depositAmount = Double.parseDouble(depositField.getText());
+                balance += depositAmount;
+                finalBalanceField.setText(Double.toString(balance));
+
+            }
+        }) ;
 
         panel = new JPanel();
-        panel.setLayout(new GridLayout(0, 2, 10, 10));
+        panel.setLayout(new GridLayout(0, 3, 10, 10));
 
         panel.add(balanceLabel);
         panel.add(balanceField);
+        panel.add(new JLabel(""));
         panel.add(depositLabel);
         panel.add(depositField);
+        panel.add(depositButton);
         panel.add(withdrawLabel);
         panel.add(withdrawField);
-        panel.add(processButton);
+        panel.add(withdrawButton);
         panel.add(finalBalanceLabel);
         panel.add(finalBalanceField);
 
